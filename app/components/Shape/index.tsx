d5f00fb822d73850a48a454cc0f9af2aa9ae7a8f
linglp <55448354+linglp@users.noreply.github.com>
2025-05-12 18:45:52 -0400

    only show cell if value is 1

--- app/components/Shape/index.tsx
+++ app/components/Shape/index.tsx
@@ -12,10 +12,9 @@ const Shape: React.FC<ShapeProps> = ({ shape }) => {
       <div className="shape">
         {shape.map((row, rowIndex) => (
           <div key={rowIndex} className="row">
+            
             {row.map((cell, colIndex) => (
-            <div key={`${rowIndex}_${colIndex}`} className={`${cell==1 ? 'is-colored-cell' : 'not-colored-cell'}`}>
-                {cell==1 ? cell : null}
-            </div>
+            cell == 1 ? <div key={`${rowIndex}_${colIndex}`} className={`${cell==1 ? 'is-colored-cell' : 'not-colored-cell'}`}> </div> : null
             ))}
           </div>
         ))}
