400b40a02f607d371bf4ed215acc80323eb1df2f
linglp <55448354+linglp@users.noreply.github.com>
2025-08-24 09:48:37 -0400

    rotate shape in a box

--- app/components/Shape/util.ts
+++ app/components/Shape/util.ts
@@ -1,137 +1,169 @@
-// the L shape family
 export function createLShape1(): number[][]{
-    return [[1, 0],
-            [1, 0],
-            [1, 1]];
+    return [[1, 0, 0],
+            [1, 1, 1],
+            [0, 0, 0]];
 };
 export function createLShape2(): number[][]{
-    return [[1, 1, 1],
-            [1, 0, 0]];
+    return [[0, 1, 1],
+            [0, 1, 0],
+            [0, 1, 0]
+        ];
 };
-
 export function createLShape3(): number[][]{
-    return [[1, 1],
-            [0, 1],
-            [0, 1]];
+    return [[0, 0, 0],
+            [1, 1, 1],
+            [0, 0, 1]
+        ];
 };
 export function createLShape4(): number[][]{
-    return [[0, 0, 1],
-            [1, 1, 1]];
+    return [[0, 1, 0],
+            [0, 1, 0],
+            [1, 1, 0]
+        ];
 };
 
 
 //the S shape family
 export function createSShape1(): number[][]{
-    return [[0, 1, 1],
-            [1, 1, 0]]; 
+    return [[1, 1, 0],
+            [0, 1, 1],
+            [0, 0, 0],
+        ]; 
 }
 export function createSShape2(): number[][]{
-    return [[1, 0],
-            [1, 1],
-            [0, 1]
+    return [[0, 0, 1],
+            [0, 1, 1],
+            [0, 1, 0]
             ]; 
 }
 export function createSShape3(): number[][]{
-    return [[0, 1, 1],
+    return [[0, 0, 0],
             [1, 1, 0],
+            [0, 1, 1],
             ]; 
 }
 export function createSShape4(): number[][]{
-    return [[1, 0],
-            [1, 1],
-            [0, 1],
+    return [[0, 1, 0],
+            [1, 1, 0],
+            [1, 0, 0],
             ]; 
 }
 
 //the Z shape
 export function createZShape1(): number[][]{
-    return [[1, 1, 0],
-            [0, 1, 1]]; 
+    return [
+        [0, 1, 1],
+        [1, 1, 0],
+        [0, 0, 0]
+
+    ]; 
 }
 export function createZShape2(): number[][]{
-    return [[0, 1],
-            [1, 1],
-            [1, 0]
-        ]; 
+    return [
+        [0, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1]
+
+    ]; 
 }
 export function createZShape3(): number[][]{
-    return [[1, 1, 0],
-            [0, 1, 1],
-        ]; 
+    return [
+        [0, 0, 0],
+        [0, 1, 1],
+        [1, 1, 0]
+
+    ]; 
 }
 export function createZShape4(): number[][]{
-    return [[0, 1],
-            [1, 1],
-            [1, 0],
+        return [
+            [1, 0, 0],
+            [1, 1, 0],
+            [0, 1, 0]
+
         ]; 
 }
 //the J shape
 export function createJShape1(): number[][]{
-    return [[1, 1, 1],
-            [0, 0, 1]]
+    return [
+        [0, 0, 1],
+        [1, 1, 1],
+        [0, 0, 0]
+        ]
 };
 export function createJShape2(): number[][]{
-    return [[0, 1],
-            [0, 1],
-            [1, 1],
+    return [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 1]
         ]
 };
 export function createJShape3(): number[][]{
     return [
-            [1, 0, 0],
-            [1, 1, 1],
+        [0, 0, 0],
+        [1, 1, 1],
+        [1, 0, 0]
         ]
 };
 export function createJShape4(): number[][]{
     return [
-            [1, 1],
-            [1, 0],
-            [1, 0]
+        [1, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
         ]
 };
 //the O shape
 export function createSquareShape1(): number[][]{
-    return [[1, 1],
-            [1, 1]]; 
+    return [
+        [1, 1, 1],
+        [1, 1, 1],
+        [1, 1, 1],
+
+        ]; 
 }
 
 //The I shape family
 export function createIShape1(): number[][]{
-    return [[1],
-            [1],
-            [1],
-            [1]
+    return [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0],
         ]; 
 }
 export function createIShape2(): number[][]{
-    return [[1, 1, 1, 1]]; 
+    return [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0],
+        ]; 
 }
 
 //The T shape family
 export function createTShape1(): number[][]{
     return [
-            [1, 1, 1],
             [0, 1, 0],
+            [1, 1, 1],
+            [0, 0, 0],
         ]
 };
 export function createTShape2(): number[][]{
     return [
-            [0, 1],
-            [1, 1],
-            [0, 1],
+            [0, 1, 0],
+            [0, 1, 1],
+            [0, 1, 0],
         ]
 };
 export function createTShape3(): number[][]{
     return [
+            [0, 0, 0],
+            [1, 1, 1],
             [0, 1, 0],
-            [1, 1, 1]
         ]
 };
 export function createTShape4(): number[][]{
     return [
-            [1, 0],
-            [1, 1],
-            [1, 0],
+            [0, 1, 0],
+            [1, 1, 0],
+            [0, 1, 0],
         ]
 };
 const allShapesFunctions = [
@@ -198,7 +230,7 @@ const shapeRegistry: { [key: string]: () => number[][] } = {
     createZShape4,
     //O shape family
     createSquareShape1,
-};
+}
 
 export function randomShapeGenerator(): number[][] {
     let randomShape:string = allShapesFunctions[Math.floor(Math.random() * allShapesFunctions.length)];
