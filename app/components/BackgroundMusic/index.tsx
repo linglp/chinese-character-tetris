a7ecabd9d9db794b834b90542faa7b2d8c3abb79
linglp <55448354+linglp@users.noreply.github.com>
2025-09-28 16:22:34 -0400

    enable music in tetris game

---
+++ app/components/BackgroundMusic/index.tsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect } from "react";
+
+type MusicProps = {
+  endGame: boolean;
+  hasInitialized: boolean;
+
+}
+
+const BackgroundMusic: React.FC<MusicProps> = ({endGame, hasInitialized}) => {
+    const audioRef = useRef<HTMLAudioElement>(null);
+
+    useEffect(() => {
+        audioRef.current = new Audio("/Yamiyo.mp3");
+        audioRef.current.loop = true;
+        audioRef.current.volume = 0.5;
+
+
+    if (!endGame && hasInitialized){
+      audioRef.current.play().catch(err => {
+      console.warn("Autoplay blocked:", err);
+      });
+    } else {
+      audioRef.current.pause();
+    }
+
+  }, [endGame, hasInitialized]);
+
+  return null;
+}
+export default BackgroundMusic
